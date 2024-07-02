398297544bf6e3536d426450b55b8d483c500cd7
omarovici <omarkhalidsaber042004@gmail.com>
2024-07-02 19:39:31 +0300

    Assignment-05

---
+++ Assignment-05/Demo.cs
@@ -0,0 +1,155 @@
+﻿using System.Text;
+
+namespace Demo;
+
+class Program
+{
+    static void Main1(string[] args)
+    {
+        #region Named and Default Input Parameters
+
+        // Print();
+        // PrintRepeatableSymbols(7,"#");
+        #endregion
+
+        #region Passing By Value & Refrence [Value Type]
+
+        // int num01 = 10, num02 = 20;
+        // Console.WriteLine($"Num01 = {num01}, Num02 = {num02}");
+        // Swap( ref num01, ref num02) ;
+        // Console.WriteLine($"Num01 = {num01}, Num02 = {num02}");
+
+        #endregion
+
+        #region Passing By Value & Reference [Reference Type] (Edit Item Value) & (Ref Keyword)
+        // int[] numbers = { 5, 7, 8, 9, 1 };
+        //
+        // Console.WriteLine($"Outer Before: {numbers.GetHashCode()}");
+        // Console.WriteLine(SumArrayElements(numbers));
+        //
+        // Console.WriteLine($"Outer After: {numbers.GetHashCode()}");
+        //
+        // Console.WriteLine(numbers[0]);
+        #endregion
+        
+        #region Passing By Out
+        // int X = 10, Y = 20, sum, multiply;
+        //
+        // SumAndMultiply(X, Y, out sum, out multiply);
+        //
+        // Console.WriteLine(sum);
+        // Console.WriteLine(multiply);
+
+        #endregion
+        
+        #region Params
+        //int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8 };
+
+        // int result = SumArrayElements (1, 2, 3, 4, 5, 6, 7, 8);
+        //
+        // Console.WriteLine(result);
+        #endregion
+
+        #region Enum
+
+        // Enum.TryParse(typeof(DaysOfWeek), Console.ReadLine(), false, out object day)
+        //
+        // Console.WriteLine(day);
+
+
+        #endregion
+
+    }
+
+    #region Named and Default Input Parameters
+
+    // static void Print()
+    // {
+    //     for (int i = 0; i < 10; i++)
+    //         Console.Write("$");
+    // }
+    //
+    // static void PrintRepeatableSymbols(int countOfIterations, string symbol)
+    // {
+    //     for (int i = 0; i < countOfIterations; i++)
+    //         Console.Write(symbol);
+    // }
+    
+    #endregion
+    
+    #region Passing By Value & Refrence [Value Type]
+
+    // static void Swap(ref int x , ref int y)
+    // {
+    //     int temp = x;
+    //     x = y;
+    //     y = temp;
+    // }
+
+    #endregion
+    
+    #region Passing By Value & Reference [Reference Type] (Edit Item Value) & (Ref Keyword)
+
+    // static int SumArrayElements(int[] numbers)
+    // {
+    //     Console.WriteLine($"Inner Before: {numbers.GetHashCode()}");
+    //     int sum = 0;
+    //
+    //     numbers[0] = 100;
+    //     Console.WriteLine($"Inner After: {numbers.GetHashCode()}");  
+    //
+    //     for (int i = 0; i < numbers.Length; i++)
+    //     {
+    //         sum += numbers[i];
+    //     }
+    //
+    //     return sum;
+    // }
+    #endregion
+
+    #region Pass By Out
+
+    // static void SumAndMultiply(int x, int y, out int sum, out int multiply)
+    //
+    // {
+    //     sum = x + y;
+    //     multiply = x * y;
+    // }
+
+    #endregion
+    
+    #region By Value Vs Reference Vs Out
+    // By Value => Read Only
+    // By Reference => Read First then Write
+    // By Out => Write First then Read
+    #endregion
+    
+    #region Params
+    // static int SumArrayElements(params int[] numbers)
+    // {
+    //     int sum = 0;
+    //     foreach (int num in numbers)
+    //     {
+    //         sum += num;
+    //     }
+    //     return sum;
+    // }
+
+    #endregion
+
+    #region Enum
+    
+    // public enum DaysOfWeek
+    // {
+    //     Sunday = 1,
+    //     Monday,
+    //     Tuesday,
+    //     Wednesday,
+    //     Thursday,
+    //     Friday,
+    //     Saturday
+    // }
+
+    #endregion
+    
+}
---
+++ Assignment-05/Program.cs
@@ -0,0 +1,129 @@
+﻿namespace Assignment_05;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // 1- Create an enum called "WeekDays" with the days of the week
+        // (Monday to Sunday) as its members. Then, write a C# program that
+        // prints out all the days of the week using this enum.
+
+        #region Problem 1
+
+        // WeekDays[] day = (WeekDays[])Enum.GetValues(typeof(WeekDays));
+        // foreach (var i in day)
+        // {
+        //     Console.WriteLine(i);
+        // }
+
+        #endregion
+        
+        // 2. Create an enum called "Season" with the four seasons (Spring,
+        // Summer, Autumn, Winter) as its members. Write a C# program that
+        // takes a season name as input from the user and displays the
+        // corresponding month range for that season. Note range for seasons
+        // (spring march to may, summer june to August, autumn September to
+        // November, winter December to February)
+
+        #region Problem 2
+
+        // Console.WriteLine("Enter the season name: ");
+        // Enum.TryParse(typeof(Season), Console.ReadLine(), true, out var s);
+        //
+        // switch (s)
+        // {
+        //     case Season.Spring:
+        //         Console.WriteLine("March to May");
+        //         break;
+        //     case Season.Summer:
+        //         Console.WriteLine("June to August");
+        //         break;
+        //     case Season.Autumn:
+        //         Console.WriteLine("September to November");
+        //         break;
+        //     case Season.Winter:
+        //         Console.WriteLine("December to February");
+        //         break;
+        //     default:
+        //         Console.WriteLine("Invalid");
+        //         break;
+        // }
+
+        #endregion
+        
+        // 3- Assign the following Permissions (Read, write, Delete, Execute) in a
+        //     form of Enum.
+        //     ● Create Variable from previous Enum to Add and Remove
+        //     Permission from variable, check if specific Permission is
+        //     existed inside variable
+
+        #region Problem 3
+
+        // Employee employee = new Employee();
+        // Console.Write("Enter the value: ");
+        // int x = int.Parse(Console.ReadLine());
+        // employee.permission = (Permission)x;
+        // Console.WriteLine($"The permissions that the employee has : {employee.permission}");
+        
+        #endregion
+
+        // 4. Create an enum called "Colors" with the basic colors (Red, Green, Blue)
+        //     as its members. Write a C# program that takes a color name as input from
+        //     the user and displays a message indicating whether the input color is a
+        //     primary color or not
+
+        #region Problem 4
+
+        // Console.WriteLine("Enter a color name: ");
+        // string x = Console.ReadLine();
+        //
+        // if (Enum.TryParse(x, true, out Colors color))
+        //     Console.WriteLine($"{x} is a primary color.");
+        // else
+        //     Console.WriteLine($"{x} is not a primary color.");
+
+        #endregion
+
+    }
+    [Flags]
+    public enum Permission
+    {
+        Read = 1,
+        Write = 2,
+        Delete = 4,
+        Execute = 8
+    }
+    public enum WeekDays
+    {
+        Monday = 1,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday
+    }
+    
+    public enum Season
+    {
+        Spring = 1,
+        Summer,
+        Autumn,
+        Winter
+    }
+    
+    enum Colors
+    {
+        Red = 1,
+        Green,
+        Blue
+    }
+
+    internal class Employee
+    {
+        public int ID;
+        public string Name;
+        public Permission permission;
+    }
+    
+}
