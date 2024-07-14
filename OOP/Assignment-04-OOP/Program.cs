7bd6802be91f7e15e1e8c5cf16468b033489e58c
omarovici <omarkhalidsaber042004@gmail.com>
2024-07-14 19:16:26 +0300

    Assignment-04 OOP

---
+++ OOP/Assignment-04-OOP/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Assignment_04_OOP;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double a, b;
+        bool check = true;
+        do
+        {
+            Console.Write("Enter the value of a: ");
+            check = double.TryParse(Console.ReadLine(), out a);
+            if (!check) Console.WriteLine("Wrong value, please enter the value again.");
+        } while (!check);
+
+        do
+        {
+            Console.Write("Enter the value of b: ");
+            check = double.TryParse(Console.ReadLine(), out b);
+            if (!check) Console.WriteLine("Wrong value, please enter the value again.");
+        } while (!check);
+        
+        Console.WriteLine("Add: " + Maths.Add(a, b));
+        Console.WriteLine("Subtract: " + Maths.Subtract(a, b));
+        Console.WriteLine("Multiply: " + Maths.Multiply(a, b));
+        Console.WriteLine("Divide: " + Maths.Divide(a, b));
+    }
+}
