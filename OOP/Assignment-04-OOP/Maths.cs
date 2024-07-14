38d2d069253861f0439fa30cdd21418ae515b8f2
omarovici <omarkhalidsaber042004@gmail.com>
2024-07-14 19:14:37 +0300

    Assignment-0٤ OOP

---
+++ OOP/Assignment-04-OOP/Maths.cs
@@ -0,0 +1,28 @@
+namespace Assignment_04_OOP;
+
+public class Maths
+{
+    public static double Add(double a, double b)
+    {
+        return a + b;
+    }
+
+    public static double Subtract(double a, double b)
+    {
+        return a - b;
+    }
+
+    public static double Multiply(double a, double b)
+    {
+        return a * b;
+    }
+
+    public static double Divide(double a, double b)
+    {
+        if (b == 0)
+        {
+            throw new DivideByZeroException("You can't divide by zero");
+        }
+        return a / b;
+    }
+}
