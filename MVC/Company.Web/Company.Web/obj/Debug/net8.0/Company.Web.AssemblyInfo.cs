c98574d600d169636dfd0e9abdaf0d0d8e41c5a2
omarovici <omarkhalidsaber042004@gmail.com>
2024-09-08 21:16:55 +0300

    Add Identity Tables to the Database

--- MVC/Company.Web/Company.Web/obj/Debug/net8.0/Company.Web.AssemblyInfo.cs
+++ MVC/Company.Web/Company.Web/obj/Debug/net8.0/Company.Web.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Company.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f319aad8922a05a8ef79bbf4918ad0cbb8291b6f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1d9ff5b185180b22faab38d384842426f7a5637")]
 [assembly: System.Reflection.AssemblyProductAttribute("Company.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Company.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
